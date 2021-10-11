725f095d12ef67fa3a90fc670cff38e07af0ad84
Fabio Beutler <dev.fabiobeutler@gmail.com>
2021-10-11 11:19:59 -0400

    testando componentes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,25 @@
 module.exports = {
-  testIgnorePatterns: ['/node_modules/', '/.next/'],
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**'
+  ],
+
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+    /* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
-  testEnvironment: 'jsdom'
+  moduleNameMapper: {
+    /* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
