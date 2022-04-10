1da6d06d19d2b57b9f29785778be542867d5e602
Fabio Beutler <dev.fabiobeutler@gmail.com>
2022-04-09 23:57:36 -0400

    Add preview on posts redirect

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,21 +1,21 @@
-import { GetServerSideProps } from 'next'
-import Head from 'next/head'
-import { getSession } from 'next-auth/client'
-import { RichText } from 'prismic-dom'
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { getSession } from "next-auth/client";
+import { RichText } from "prismic-dom";
 
-import { getPrismicClient } from '../../services/prismic'
+import { getPrismicClient } from "../../services/prismic";
 
-import styles from './post.module.scss'
+import styles from "./post.module.scss";
 
 type Post = {
-  slug: string
-  title: string
-  content: string
-  updatedAt: string
-}
+  slug: string;
+  title: string;
+  content: string;
+  updatedAt: string;
+};
 
 interface PostProps {
-  post: Post
+  post: Post;
 }
 
 export default function Post({ post }: PostProps) {
@@ -36,47 +36,46 @@ export default function Post({ post }: PostProps) {
         </article>
       </main>
     </>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
   req,
-  params
+  params,
 }) => {
-  const session = await getSession({ req })
-  const { slug } = params
+  const session = await getSession({ req });
+  const { slug } = params;
 
-  console.log(session)
+  console.log(session);
 
   if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: `/posts`,
-        permanent: false
-      }
-    }
+        destination: `/posts/preview/${slug}`,
+        permanent: false,
+      },
+    };
   }
 
-  const prismic = getPrismicClient(req)
+  const prismic = getPrismicClient(req);
 
-  const response = await prismic.getByUID('publication', String(slug), {})
-
+  const response = await prismic.getByUID("publication", String(slug), {});
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString(
-      'pt-BR',
+      "pt-BR",
       {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
       }
-    )
-  }
+    ),
+  };
 
   return {
-    props: { post }
-  }
-}
+    props: { post },
+  };
+};
