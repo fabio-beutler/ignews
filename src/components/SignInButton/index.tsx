bf7ee95ff432d1c5a621b9a0fae6d550a6948fa1
Fabio Beutler <dev.fabiobeutler@gmail.com>
2022-04-10 00:40:37 -0400

    Remove default providor on signin button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,30 +1,30 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
-import { signIn, signOut, useSession } from 'next-auth/client'
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [session] = useSession()
+  const [session] = useSession();
 
   return session ? (
     <button
       className={styles.signInButton}
-      type='button'
+      type="button"
       onClick={() => signOut()}
     >
-      <FaGithub color='#04d361' />
+      <FaGithub color="#04d361" />
       {session.user.name}
-      <FiX color='#737380' className={styles.closeIcon} />
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       className={styles.signInButton}
-      type='button'
-      onClick={() => signIn('github')}
+      type="button"
+      onClick={() => signIn()}
     >
-      <FaGithub color='#eba417' />
+      <FaGithub color="#eba417" />
       Sign in with Github
     </button>
-  )
+  );
 }
